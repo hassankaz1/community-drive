ca53b7671d6b518927c2d283e6433d24bf158904
Hassan Kazi <andaijja@gmail.com>
2023-04-01 16:53:35 -0400

    fixing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import { createContext, useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from './Components/Login/Login';
+import Home from './Components/Home/Home'
+import { db, auth } from "./firebase.js";
+import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import Dashboard from './Components/User/Dashboard/Dashboard';
+import Gpt3Component from "./Components/Gpt/Gpt"
+
+
+
+export const AuthContext = createContext(null);
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+
+    async function getUserInfo(uid) {
+      const userRef = doc(db, "users", uid);
+      const docSnap = await getDoc(userRef);
+
+
+      if (docSnap.exists()) {
+        const user = docSnap.data();
+        user["uid"] = uid
+        setCurrentUser(user)
+      } else {
+        console.log("No such document!");
+      }
+
+    }
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        getUserInfo(user.uid)
+
+
+      } else {
+        setCurrentUser(null);
+      }
+    });
+  }, []);
+
+  // useEffect(() => {
+
+  //   async function getUserInfo() {
+  //     const userRef = doc(db, "users", currentUser.uid);
+  //     const docSnap = await getDoc(userRef);
+
+
+  //     if (docSnap.exists()) {
+  //       const user = docSnap.data();
+  //       user["uid"] = currentUser.uid
+  //       setCurrentUser(user)
+  //     } else {
+  //       console.log("No such document!");
+  //     }
+
+  //   }
+
+
+  //   if (currentUser) {
+  //     getUserInfo()
+  //   }
+  // }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value={{ currentUser, setCurrentUser }}>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/gpt' element={<Gpt3Component />}></Route>
+      </Routes>
+    </AuthContext.Provider>
   );
 }
 
